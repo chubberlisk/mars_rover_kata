6f5e47a30f8d8d59851996e84cf8c300565127e2
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-23 21:48:06 +0000

    feat: move forward and backward

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -5,4 +5,18 @@ class MarsRover
     @position = starting_position
     @direction = facing_direction
   end
+
+  def move(commands)
+    commands.each do |command|
+      if @direction == :north
+        command == 'f' ? @position[:y] += 1 : @position[:y] -= 1
+      elsif @direction == :east
+        command == 'f' ? @position[:x] += 1 : @position[:x] -= 1
+      elsif @direction == :south
+        command == 'f' ? @position[:y] -= 1 :  @position[:y] += 1
+      else
+        command == 'f' ? @position[:x] -= 1 : @position[:x] += 1
+      end
+    end
+  end
 end
