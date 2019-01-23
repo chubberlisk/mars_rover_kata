5c967aea80161bd1f1ca8c3c98a914c39d1add47
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-23 19:33:48 +0000

    feat: initialise with starting position and facing direction

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,2 +1,8 @@
 class MarsRover
+  attr_reader :position, :direction
+
+  def initialize(starting_position, facing_direction)
+    @position = starting_position
+    @direction = facing_direction
+  end
 end
