4cea390092c6c156f48985316e80171b97cb74e0
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-23 19:33:35 +0000

    test: add test for initialisation

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,5 +1,17 @@
 require_relative '../lib/mars_rover'
 
 describe MarsRover do
-  
+  subject { MarsRover.new([0, 0], :north) }
+
+  it 'takes an initial starting position and direction' do
+    expect(MarsRover).to respond_to(:new).with(2).arguments
+  end
+
+  it 'sets initial starting position to current position' do
+    expect(subject.position).to eq([0, 0])
+  end
+
+  it 'sets initial facing direction to current direction' do
+    expect(subject.direction).to eq(:north)
+  end
 end
